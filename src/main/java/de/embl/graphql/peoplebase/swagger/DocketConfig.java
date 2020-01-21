dbeef62a32bac9a4c0de46adfffaaa4ce77ec85b
Supun <hsupunw@gmail.com>
2020-01-21 20:42:56 +0530

    docket config

--- src/main/java/de/embl/graphql/peoplebase/swagger/DocketConfig.java
+++ src/main/java/de/embl/graphql/peoplebase/swagger/DocketConfig.java
@@ -2,9 +2,7 @@ package de.embl.graphql.peoplebase.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
